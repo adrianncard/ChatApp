3e7d6a82837c1a49ac58a522b120655919535bf8
adrianncard <adrianncarb@gmail.com>
2021-11-24 19:37:15 -0800

    getting started

---
+++ ChatApp/Controllers/ConversationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Adrian Cardenas on 2021-11-24.
+//
+
+import UIKit
+
+class ConversationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
