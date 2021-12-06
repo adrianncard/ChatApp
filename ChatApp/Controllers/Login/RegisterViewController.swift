a41d147f853f56720e55d74281082d17d41b4aa5
adrianncard <adrianncarb@gmail.com>
2021-12-05 22:07:50 -0800

    adding choose and take photo

--- ChatApp/Controllers/Login/RegisterViewController.swift
+++ ChatApp/Controllers/Login/RegisterViewController.swift
@@ -138,7 +138,7 @@ class RegisterViewController: UIViewController {
     }
     
     @objc private func didTapChangeProfilePic(){
-        print("change pic called")
+        presentPhotoActionSheet()
     }
     
     override func viewDidLayoutSubviews() {
@@ -235,3 +235,38 @@ extension RegisterViewController: UITextFieldDelegate {
     
 }
 
+extension RegisterViewController: UIImagePickerControllerDelegate {
+    
+   
+    func presentPhotoActionSheet(){
+        let actionSheet = UIAlertController (title: "profile picture",
+                                             message: "How would you like to select your picture ?",
+                                             preferredStyle: .actionSheet)
+        actionSheet .addAction(UIAlertAction(title: "cancel",
+                                             style: .cancel,
+                                             handler: nil))
+        actionSheet .addAction(UIAlertAction(title: "Take Photo",
+                                             style: .default,
+                                             handler: { _ in
+            
+        }))
+        actionSheet .addAction(UIAlertAction(title: "Choose Photo",
+                                             style: .default,
+                                             handler: {_ in
+            
+        }))
+        
+        present (actionSheet, animated: true)
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+    }
+    
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+    }
+    
+}
